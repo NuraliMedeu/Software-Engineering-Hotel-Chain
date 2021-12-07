3e6c8fe92c7f3d17d9e03355b5cacdc61fba1e07
Islambek <islambek.mamet.00@gmail.com>
2021-12-07 19:12:20 +0600

    a

--- auth.js
+++ auth.js
@@ -1,5 +1,18 @@
 $(document).ready(function () {
   $("#signin-btn").on("click", function (e) {
-    window.location.href = "../account/user-account.html";
+    let select = document.getElementById("#select-user");
+    switch (select.value) {
+      case "User":
+        window.location.href = "../account/user-account.html";
+        break;
+      case "Desk clerk":
+        window.location.href = "../account/dc-account.html";
+        break;
+      case "Manager":
+        window.location.href = "../account/manager-account.html";
+        break;
+      default:
+        break;
+    }
   });
 });
