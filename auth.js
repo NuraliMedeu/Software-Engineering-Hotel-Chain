df645a97c88c66a5372a8a6eb2a57cc4db5bc8c6
Islambek <islambek.mamet.00@gmail.com>
2021-12-08 18:01:15 +0600

    fixed path

--- auth.js
+++ auth.js
@@ -1,4 +1,39 @@
 $(document).ready(function () {
+  $("#signup-btn").on("click", function (e) {
+    e.preventDefault();
+    var form = document.getElementById("signup-form");
+    var elements = form.elements;
+    var formData = {};
+    for (var i = 0; i < elements.length; i++) {
+      var item = elements.item(i);
+      if (item.name === "agree") {
+        formData[item.name] = item.checked;
+        if (!item.checked) {
+          form.reportValidity();
+          return;
+        }
+      } else formData[item.name] = item.value;
+    }
+
+    if (formData.password !== formData.password_confirmation)
+      return alert("Passwords must be the same");
+
+    $.post(
+      "http://localhost:8080/api/guest",
+      {
+        name: formData.name,
+        surname: formData.surname,
+        email: formData.email,
+        password: formData.password,
+        id_typeNum: formData.id_type,
+        id_number: formData.id_number,
+      },
+      function (result) {
+        console.log("res = ", result);
+      }
+    );
+  });
+
   $("#signin-btn").on("click", function (e) {
     let select = document.getElementById("#select-user");
     switch (select.value) {
@@ -15,4 +50,8 @@ $(document).ready(function () {
         break;
     }
   });
+
+  const onSignup = (e) => {
+    console.log("e = ", e.target);
+  };
 });
