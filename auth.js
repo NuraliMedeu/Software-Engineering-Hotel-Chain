f8e373e8f49a0a90acb496d4a30a7843a4f2ad57
Islambek <islambek.mamet.00@gmail.com>
2021-12-08 22:31:24 +0600

    auth js

--- auth.js
+++ auth.js
@@ -36,6 +36,27 @@ $(document).ready(function () {
 
   $("#signin-btn").on("click", function (e) {
     let select = document.getElementById("#select-user");
+
+    e.preventDefault();
+    var form = document.getElementById("signin-form");
+    var elements = form.elements;
+    var formData = {};
+    for (var i = 0; i < elements.length; i++) {
+      var item = elements.item(i);
+      formData[item.name] = item.value;
+    }
+
+    $.post(
+      "http://localhost:8080/api/guest",
+      {
+        email: formData.email,
+        password: formData.password,
+      },
+      function (result) {
+        console.log("res = ", result);
+      }
+    );
+
     switch (select.value) {
       case "User":
         window.location.href = "../account/user-account.html";
