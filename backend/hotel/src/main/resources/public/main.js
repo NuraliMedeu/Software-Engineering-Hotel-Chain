76ba1ce2866f0e984b2512ddf0d3d9615ffe4e16
Islambek <islambek.mamet.00@gmail.com>
2021-12-09 17:34:12 +0600

    finished auth process

--- backend/hotel/src/main/resources/public/main.js
+++ backend/hotel/src/main/resources/public/main.js
@@ -1,4 +1,51 @@
 $(document).ready(function () {
+  if (localStorage.getItem("user_data")) {
+    var navbarUsername = document.getElementsByClassName("flex-center");
+    let userData = JSON.parse(localStorage.getItem("user_data"));
+    var username = document.getElementsByClassName("username");
+    var usertype = document.getElementsByClassName("usertype");
+    var userDataInProfile = document.getElementsByClassName("user-account");
+    Array.from(username).forEach((item) => {
+      item.textContent = userData?.name;
+    });
+
+    Array.from(usertype).forEach((item) => {
+      item.textContent = userData?.user_type?.type.toString().toUpperCase();
+    });
+    Array.from(userDataInProfile).forEach((item) => {
+      let divContent = ``;
+      Object.keys(userData).forEach(function (key) {
+        var val = userData[key];
+        divContent += `<div class="field"><span>${key}:</span><span>${val}</span></div>`;
+      });
+      item.innerHtml = divContent;
+      console.log("item = ", item);
+      console.log("div = ", divContent);
+    });
+
+    Array.from(navbarUsername).forEach((item) => {
+      item.style.cursor = "pointer";
+      var htmlEl = `<img src="../../static/images/user.png" alt="user" class="user-img-navbar" /><span style="color: white">${userData?.name}</span>`;
+      item.innerHTML = htmlEl;
+      console.log("item = ", item);
+      item.onclick = (e) => {
+        switch (userData?.user_type?.type) {
+          case "guest":
+            window.location.href = "pages/account/user-account.html";
+            break;
+          case "desk clerk":
+            window.location.href = "pages/account/dc-account.html";
+            break;
+          case "manager":
+            window.location.href = "pages/account/manager-account.html";
+            break;
+          default:
+            break;
+        }
+      };
+    });
+  }
+
   $("#check-in input").datepicker({
     autoclose: true,
   });
