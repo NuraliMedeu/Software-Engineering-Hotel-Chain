7fb8fb5c8031a7a1c0ae3af769aa9b7e063c944f
Nurali Medeu <nurali.medeu01@gmail.com>
2021-12-09 17:35:51 +0600

    Reformatted some files

--- backend/hotel/src/main/resources/public/main.js
+++ backend/hotel/src/main/resources/public/main.js
@@ -1,45 +1,52 @@
-$(document).ready(function () {
-  $("#check-in input").datepicker({
-    autoclose: true,
-  });
-
-  $("#check-in").on("show", function (e) {
-    if (e.date) {
-      $(this).data("stickyDate", e.date);
-    } else {
-      $(this).data("stickyDate", null);
-    }
-  });
-
-  $("#check-in").on("hide", function (e) {
-    var stickyDate = $(this).data("stickyDate");
-
-    if (!e.date && stickyDate) {
-      console.debug("restore stickyDate", stickyDate);
-      $(this).datepicker("setDate", stickyDate);
-      $(this).data("stickyDate", null);
-    }
-  });
-
-  $("#check-out input").datepicker({
-    autoclose: true,
-  });
-
-  $("#check-out").on("show", function (e) {
-    if (e.date) {
-      $(this).data("stickyDate", e.date);
-    } else {
-      $(this).data("stickyDate", null);
-    }
-  });
-
-  $("#check-out").on("hide", function (e) {
-    var stickyDate = $(this).data("stickyDate");
-
-    if (!e.date && stickyDate) {
-      console.debug("restore stickyDate", stickyDate);
-      $(this).datepicker("setDate", stickyDate);
-      $(this).data("stickyDate", null);
-    }
-  });
+$(document).ready(function() {
+    $("#check-in input").datepicker({
+        autoclose: true
+    });
+    $("#check-in").on("show", function(e) {
+        if (e.date) {
+            $(this).data("stickyDate", e
+                .date);
+        } else {
+            $(this).data("stickyDate",
+                null);
+        }
+    });
+    $("#check-in").on("hide", function(e) {
+        var stickyDate = $(this).data(
+            "stickyDate");
+        if (!e.date && stickyDate) {
+            console.debug(
+                "restore stickyDate",
+                stickyDate);
+            $(this).datepicker("setDate",
+                stickyDate);
+            $(this).data("stickyDate",
+                null);
+        }
+    });
+    $("#check-out input").datepicker({
+        autoclose: true
+    });
+    $("#check-out").on("show", function(e) {
+        if (e.date) {
+            $(this).data("stickyDate", e
+                .date);
+        } else {
+            $(this).data("stickyDate",
+                null);
+        }
+    });
+    $("#check-out").on("hide", function(e) {
+        var stickyDate = $(this).data(
+            "stickyDate");
+        if (!e.date && stickyDate) {
+            console.debug(
+                "restore stickyDate",
+                stickyDate);
+            $(this).datepicker("setDate",
+                stickyDate);
+            $(this).data("stickyDate",
+                null);
+        }
+    });
 });
